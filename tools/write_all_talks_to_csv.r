9065f6ba0396f04ed2ea3cd0a541631839f73e22
Rufibach <rufiback@emea.roche.com>
2022-02-11 19:44:24 +0100

    update

--- tools/write_all_talks_to_csv.r
+++ tools/write_all_talks_to_csv.r
@@ -1,7 +1,7 @@
 path <- getwd()
 
 # list all talks in a directory
-tab <- data.frame(list.files(paste(path, "/talks/20160113", sep = "")))
+tab <- data.frame(list.files(paste(path, "/talks/20141002", sep = "")))
 tab <- tab[tab[, 1] != "agenda.pdf", ]
 dimnames(tab) <- NULL
 write.table(tab, file = "C:/Users/rufiback/Desktop/xxx.csv", row.names = FALSE, col.names = FALSE)
